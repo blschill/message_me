28f514f878f28ed7878e521cef2c4f5ea11953ee
Brady Schiller <blschill@ucsd.edu>
2022-01-25 16:22:18 -0800

    Create user and message models with limitationa and one-to-many association

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "mashrur", password: "password")
+User.create(username: "jonnyboy", password: "password")
+User.create(username: "janedoe", password: "password")
+User.create(username: "bradyschiller", password: "password")
+User.create(username: "tonysoprano", password: "password")
+User.create(username: "greghibbs1", password: "password")
