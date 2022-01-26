c1a415cc1d5f411de0aecad5ebd7eaac342cd0b7
Brady Schiller <blschill@ucsd.edu>
2022-01-26 13:54:09 -0800

    Add layout for flash messages using Semantic Ui class names

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
-      byebug
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in!"
       redirect_to root_path
